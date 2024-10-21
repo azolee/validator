bdc5ce6ff449735ddd4b116bbc49726f65f6aa37
Andras Zoltan <azolee@gmail.com>
2024-10-21 23:49:41 +0300

    Prepare refactor the Validator to be configurable and also to align to Single responsability

---
+++ src/ValidationErrorManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Azolee\Validator;
+
+use Azolee\Validator\Contracts\ValidationErrorBagInterface;
+
+class ValidationErrorManager
+{
+    protected ValidationResult $validationResult;
+
+    public function __construct(ValidationErrorBagInterface $validationErrorBag = null)
+    {
+        $this->validationResult = new ValidationResult($validationErrorBag ?? new ValidationErrorBag());
+    }
+
+    public function setFailed(string $rule, string $key, mixed $dataToValidate, ?string $message = null): void
+    {
+        $this->validationResult->setFailed($rule, $key, $dataToValidate, $message);
+    }
+
+    public function getValidationResult(): ValidationResult
+    {
+        return $this->validationResult;
+    }
+}
